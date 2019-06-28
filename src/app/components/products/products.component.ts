622f3221e06a5687eb980d76c03a7868168cafd8
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-06-28 14:34:11 +0100

    some change in a function

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -30,10 +30,14 @@ export class ProductsComponent implements OnInit {
     }).catch(err => console.log(err.message));
   }
 
+  // changeEtatProduct(product) {
+  //   this.productService.activeProduct(product).then(() => { }).catch(err => console.error(err));
+  // }
+
   changeEtatProduct(product) {
-    this.productService.activeProduct(product).then(() => { }).catch(err => console.error(err));
+    this.productService.update(product.id, {
+      active: !product.active
+    }).then(() => { }).catch(err => console.error(err));
   }
-
-
 
 }
